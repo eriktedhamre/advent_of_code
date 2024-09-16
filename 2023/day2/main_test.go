c644f808387e2fb22779205b4e8301fd5d3f39f7
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2024-09-16 18:56:03 +0200

    2023 Day 2, Part 2

--- 2023/day2/main_test.go
+++ 2023/day2/main_test.go
@@ -6,6 +6,30 @@ import (
 	"testing"
 )
 
+func Test_partTwo(t *testing.T) {
+
+	tests := []struct {
+		filepath string
+		expected uint64
+	}{
+		{"example.txt", 2286},
+	}
+
+	for _, tt := range tests {
+		file, err := os.Open(tt.filepath)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+		result := partTwo(file)
+		if result != tt.expected {
+			t.Errorf("Expected: %d, got: %d, file: %s",
+				tt.expected, result, tt.filepath)
+		}
+	}
+}
+
 func Test_partOne(t *testing.T) {
 
 	tests := []struct {
