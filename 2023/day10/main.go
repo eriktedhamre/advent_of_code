b1ecc49d2b7291450b120ff4ecda2049bf3153b3
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2024-10-21 22:15:06 +0200

    2023 day 10 part one

--- 2023/day10/main.go
+++ 2023/day10/main.go
@@ -11,7 +11,10 @@ type Pos struct {
 	col int
 }
 
-var moves []Pos = []Pos{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+var north Pos = Pos{-1, 0}
+var south Pos = Pos{1, 0}
+var east Pos = Pos{0, 1}
+var west Pos = Pos{0, -1}
 
 func main() {
 
@@ -30,7 +33,6 @@ func main() {
 }
 
 func partOne(file *os.File) int64 {
-	var sum int64
 	var tiles [][]rune
 	var visited [][]bool
 	var line string
@@ -38,7 +40,9 @@ func partOne(file *os.File) int64 {
 	var S_col int
 	var rows int
 	var cols int
-	var loop_length int = 0
+	var goal bool
+	var moveA Pos
+	var moveB Pos
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -57,8 +61,8 @@ func partOne(file *os.File) int64 {
 
 FIND_S:
 	for i, row := range tiles {
-		for j, col := range row {
-			if row[col] == 'S' {
+		for j, _ := range row {
+			if row[j] == 'S' {
 				S_row = i
 				S_col = j
 				break FIND_S
@@ -68,46 +72,84 @@ FIND_S:
 
 	visited[S_row][S_col] = true
 
-	stack := make([]Pos, 0)
+	loop := make([]Pos, 0)
+	loop = append(loop, Pos{S_row, S_col})
 
 	switch {
 	// South -> North
 	case inBound(S_row+1, S_col, rows, cols) && ((tiles[S_row+1][S_col] == '|') ||
 		(tiles[S_row+1][S_col] == 'L') ||
 		(tiles[S_row+1][S_col] == 'J')):
-		stack = append(stack, Pos{S_row + 1, S_col})
+		loop = append(loop, Pos{S_row + 1, S_col})
 		visited[S_row+1][S_col] = true
 	// North -> South
 	case inBound(S_row-1, S_col, rows, cols) && ((tiles[S_row-1][S_col] == '|') ||
 		(tiles[S_row-1][S_col] == '7') ||
 		(tiles[S_row-1][S_col] == 'F')):
-		stack = append(stack, Pos{S_row - 1, S_col})
+		loop = append(loop, Pos{S_row - 1, S_col})
 		visited[S_row-1][S_col] = true
 	// East -> West
 	case inBound(S_row, S_col+1, rows, cols) && ((tiles[S_row][S_col+1] == '-') ||
 		(tiles[S_row][S_col+1] == 'J') ||
 		(tiles[S_row][S_col+1] == '7')):
-		stack = append(stack, Pos{S_row, S_col + 1})
+		loop = append(loop, Pos{S_row, S_col + 1})
 		visited[S_row][S_col+1] = true
 	// West -> East
 	case inBound(S_row, S_col-1, rows, cols) && ((tiles[S_row][S_col-1] == '-') ||
 		(tiles[S_row][S_col-1] == 'L') ||
 		(tiles[S_row][S_col-1] == 'F')):
-		stack = append(stack, Pos{S_row, S_col - 1})
+		loop = append(loop, Pos{S_row, S_col - 1})
 		visited[S_row][S_col-1] = true
 	}
-	loop_length++
 
 	// If both nodes are visited we are back where we started :) I'm a winner
-	for len(stack) > 0 {
+DONE:
+	for {
+		tail := loop[len(loop)-1]
 		switch {
-		case stack[0] == '':
-
+		case tiles[tail.row][tail.col] == '|':
+			moveA = north
+			moveB = south
+		case tiles[tail.row][tail.col] == '-':
+			moveA = east
+			moveB = west
+		case tiles[tail.row][tail.col] == 'L':
+			moveA = north
+			moveB = east
+		case tiles[tail.row][tail.col] == 'J':
+			moveA = north
+			moveB = west
+		case tiles[tail.row][tail.col] == '7':
+			moveA = south
+			moveB = west
+		case tiles[tail.row][tail.col] == 'F':
+			moveA = south
+			moveB = east
+		default:
+			fmt.Println("This is a mistake")
+		}
+		if loop, goal = nextStepReachesGoal(tail, moveA, moveB, loop, visited); goal {
+			break DONE
 		}
 
 	}
 
-	return sum
+	return int64((len(loop) + 1) / 2)
+}
+
+func nextStepReachesGoal(pos, moveA, moveB Pos, loop []Pos, visited [][]bool) ([]Pos, bool) {
+	Result := false
+	if visited[pos.row+moveA.row][pos.col+moveA.col] &&
+		visited[pos.row+moveB.row][pos.col+moveB.col] {
+		Result = true
+	} else if visited[pos.row+moveA.row][pos.col+moveA.col] {
+		loop = append(loop, Pos{pos.row + moveB.row, pos.col + moveB.col})
+		visited[pos.row+moveB.row][pos.col+moveB.col] = true
+	} else {
+		loop = append(loop, Pos{pos.row + moveA.row, pos.col + moveA.col})
+		visited[pos.row+moveA.row][pos.col+moveA.col] = true
+	}
+	return loop, Result
 }
 
 func inBound(row, col, rows, cols int) bool {
