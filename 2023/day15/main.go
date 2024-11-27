99e87cbbfca29d9417c4e94d8f6e11823b6397d5
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2024-11-27 21:08:50 +0100

    Part two aswell

--- 2023/day15/main.go
+++ 2023/day15/main.go
@@ -4,8 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Lens struct {
+	label string
+	lens  int
+}
 
 func main() {
 
@@ -20,7 +26,7 @@ func main() {
 		return
 	}
 	defer file.Close()
-	fmt.Print(partOne(file))
+	fmt.Print(partTwo(file))
 }
 
 func partOne(file *os.File) uint64 {
@@ -37,6 +43,95 @@ func partOne(file *os.File) uint64 {
 	}
 
 	return res
+}
+
+func partTwo(file *os.File) uint64 {
+	var line string
+	var initSeq []string = make([]string, 0)
+	var label string
+	var op rune
+	var number int
+	var boxIndex uint64
+
+	var boxes [][]Lens = make([][]Lens, 256)
+	for i := 0; i < len(boxes); i++ {
+		boxes[i] = make([]Lens, 0)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	line = scanner.Text()
+	initSeq = strings.Split(line, ",")
+	for _, v := range initSeq {
+		label, op, number = splitCommand(v)
+		boxIndex = hashAlgorithm(label)
+
+		if op == '=' {
+			boxes[boxIndex] = equalOp(label, boxes[boxIndex], number)
+		} else {
+			boxes[boxIndex] = dashOp(label, boxes[boxIndex])
+		}
+	}
+
+	return calculateFocusingPower(boxes)
+}
+
+func equalOp(label string, box []Lens, number int) []Lens {
+	var exists bool = false
+DONE:
+	for i := 0; i < len(box); i++ {
+		if box[i].label == label {
+			box[i].lens = number
+			exists = true
+			break DONE
+		}
+	}
+	if !exists {
+		box = append(box, Lens{label: label, lens: number})
+	}
+	return box
+}
+
+func dashOp(label string, box []Lens) []Lens {
+	var removeIndex int = -1
+
+DONE:
+	for i := 0; i < len(box); i++ {
+		if box[i].label == label {
+			removeIndex = i
+			break DONE
+		}
+	}
+
+	if removeIndex != -1 {
+		return append(box[:removeIndex], box[removeIndex+1:]...)
+	} else {
+		return box
+	}
+}
+
+func calculateFocusingPower(boxes [][]Lens) uint64 {
+	var sum uint64 = 0
+	for i, box := range boxes {
+		for j, lens := range box {
+			sum += uint64((i + 1)) * uint64(j+1) * uint64(lens.lens)
+		}
+	}
+	return sum
+}
+
+func splitCommand(s string) (string, rune, int) {
+	var tmp []string
+	if strings.Contains(s, "=") {
+		tmp = strings.Split(s, "=")
+		number, err := strconv.Atoi(tmp[1])
+		if err != nil {
+			fmt.Errorf("Unsuccessfull Atoi for %s", tmp[1])
+		}
+		return tmp[0], '=', number
+	} else {
+		return strings.TrimSuffix(s, "-"), '-', -1
+	}
 }
 
 func hashAlgorithm(s string) uint64 {
