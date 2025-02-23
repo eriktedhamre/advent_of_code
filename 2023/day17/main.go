02458924be707900b81c44b4789e5fe9b869f859
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2025-02-23 13:39:17 +0100

    2023 day 17 part two

--- 2023/day17/main.go
+++ 2023/day17/main.go
@@ -56,16 +56,8 @@ func main() {
 		return
 	}
 	defer file.Close()
-	fmt.Print(partOne(file))
+	fmt.Print(partTwo(file))
 }
-
-// Unique for a state
-// position in grid
-// current heat accumulated
-// consecutive moves
-// origin
-
-// Priority queue on heat??
 
 func partOne(file *os.File) int {
 	var line string
@@ -85,13 +77,28 @@ func partOne(file *os.File) int {
 	return searchGrid(grid)
 }
 
-// func searchGrid(grid [][]int, cost [][]int) int {
+func partTwo(file *os.File) int {
+	var line string
+	var grid [][]int = make([][]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line = scanner.Text()
+		lineSlice, err := utils.StringToIntSlice(line)
+		if err != nil {
+			fmt.Print(err)
+			panic(err)
+		}
+		grid = append(grid, lineSlice)
+	}
+
+	return searchGrid2(grid)
+}
+
 func searchGrid(grid [][]int) int {
 
-	//var curItem utils.Item[Crucible]
 	var newConsec int
 	var lowestHeat int
-	//var best *utils.Item[types.Crucible]
 	pq := make(utils.PriorityQueue[types.Crucible], 0)
 	pq.Init()
 
@@ -101,7 +108,6 @@ func searchGrid(grid [][]int) int {
 			Col:              0,
 			ConsecutiveMoves: 0,
 			Direction:        types.Down,
-			//Parents:          []types.Coordinates{{Row: 0, Col: 0}},
 		},
 		Priority: 0})
 
@@ -111,7 +117,6 @@ DONE:
 	for {
 		// We exit when we reach the goal
 		curItem, ok := heap.Pop(&pq).(*utils.Item[types.Crucible])
-		//fmt.Println(curItem.Priority)
 		if !ok {
 			fmt.Printf("type conversion failed for curItem : got %T\n", curItem)
 			panic(nil)
@@ -119,7 +124,6 @@ DONE:
 
 		if curItem.Value.Row == (len(grid)-1) && curItem.Value.Col == (len(grid[0])-1) {
 			lowestHeat = curItem.Priority
-			//best = curItem
 			break DONE
 		}
 		newIndicies := calculateNewPosSlice(curItem, len(grid), len(grid[0]))
@@ -130,13 +134,9 @@ DONE:
 			} else {
 				newConsec = 1
 			}
-			//newDir = calcNewDir(curItem.Value.Direction, i)
 
 			key := fmt.Sprintf("%d,%d,%d,%d", v.row, v.col, newConsec, v.dir)
 			newCost := curItem.Priority + grid[v.row][v.col]
-
-			// newParents := deepCopy(curItem.Value.Parents)
-			// newParents = append(newParents, types.Coordinates{Row: v.row, Col: v.col})
 
 			if prevCost, exists := bestCost[key]; !exists || (newCost < prevCost) {
 				bestCost[key] = newCost
@@ -146,7 +146,80 @@ DONE:
 						Col:              v.col,
 						ConsecutiveMoves: newConsec,
 						Direction:        v.dir,
-						//Parents:          newParents,
+					},
+					Priority: newCost,
+				})
+			}
+
+		}
+	}
+
+	return lowestHeat
+}
+
+func searchGrid2(grid [][]int) int {
+
+	var newConsec int
+	var lowestHeat int
+	pq := make(utils.PriorityQueue[types.Crucible], 0)
+	pq.Init()
+
+	heap.Push(&pq, &utils.Item[types.Crucible]{
+		Value: types.Crucible{
+			Row:              0,
+			Col:              0,
+			ConsecutiveMoves: 0,
+			Direction:        types.Down,
+		},
+		Priority: 0})
+	bestCost[fmt.Sprintf("%d,%d,%d,%d", 0, 0, 0, int(types.Down))] = 0
+
+	heap.Push(&pq, &utils.Item[types.Crucible]{
+		Value: types.Crucible{
+			Row:              0,
+			Col:              0,
+			ConsecutiveMoves: 0,
+			Direction:        types.Right,
+		},
+		Priority: 0})
+	bestCost[fmt.Sprintf("%d,%d,%d,%d", 0, 0, 0, int(types.Right))] = 0
+
+DONE:
+	for {
+		// We exit when we reach the goal
+		curItem, ok := heap.Pop(&pq).(*utils.Item[types.Crucible])
+		if !ok {
+			fmt.Printf("type conversion failed for curItem : got %T\n", curItem)
+			panic(nil)
+		}
+
+		// We need to have atleast 4 consecutive moves to stop
+		if curItem.Value.Row == (len(grid)-1) &&
+			curItem.Value.Col == (len(grid[0])-1) &&
+			curItem.Value.ConsecutiveMoves > 3 {
+			lowestHeat = curItem.Priority
+			break DONE
+		}
+		newIndicies := calculateNewPosSlice2(curItem, len(grid), len(grid[0]))
+		for _, v := range newIndicies {
+			// We are moving forward, not cash money solution
+			if v.dir == curItem.Value.Direction {
+				newConsec = curItem.Value.ConsecutiveMoves + 1
+			} else {
+				newConsec = 1
+			}
+
+			key := fmt.Sprintf("%d,%d,%d,%d", v.row, v.col, newConsec, v.dir)
+			newCost := curItem.Priority + grid[v.row][v.col]
+
+			if prevCost, exists := bestCost[key]; !exists || (newCost < prevCost) {
+				bestCost[key] = newCost
+				heap.Push(&pq, &utils.Item[types.Crucible]{
+					Value: types.Crucible{
+						Row:              v.row,
+						Col:              v.col,
+						ConsecutiveMoves: newConsec,
+						Direction:        v.dir,
 					},
 					Priority: newCost,
 				})
@@ -184,56 +257,30 @@ func calculateNewPosSlice(current *utils.Item[types.Crucible], rows, cols int) [
 	return res
 }
 
-// func calcNewDir(dir types.Direction, index int) types.Direction {
-// 	var res types.Direction
-// 	switch {
-// 	case index == 2:
-// 		res = dir
-// 	// Left or Up
-// 	case index == 0:
-// 		if dir == types.Up || dir == types.Down {
-// 			res = types.Left
-// 		} else {
-// 			res = types.Up
-// 		}
-// 	// Right or Down
-// 	case index == 1:
-// 		if dir == types.Up || dir == types.Down {
-// 			res = types.Right
-// 		} else {
-// 			res = types.Down
-// 		}
-// 	}
-// 	return res
-// }
+func calculateNewPosSlice2(current *utils.Item[types.Crucible], rows, cols int) []Pos {
+	// Look at consecutive moves and see what turns are allowed
+	// Map direction we came from to allowed turns
+	// See if new position is InBound
+	var res []Pos = make([]Pos, 0)
+	var mods []DirMod = make([]DirMod, 0)
+	var newRow int
+	var newCol int
 
-// func deepCopy(parents []types.Coordinates) []types.Coordinates {
-// 	newParents := make([]types.Coordinates, len(parents))
-// 	copy(newParents, parents)
-// 	return newParents
-// }
+	if current.Value.ConsecutiveMoves < 10 {
+		mods = append(mods, ForwardMap[current.Value.Direction])
+	}
 
-// if currentHeat + cost is lower than the previous cost add it to priority queue
-// Can this destroy the best solution by locking us out of it?
-// Don't think so, it is possible that we should simply add everything to the queue
-// I'm adding everything :)
+	if current.Value.ConsecutiveMoves > 3 {
+		mods = append(mods, LeftRightMap[current.Value.Direction]...)
+	}
 
-// for _, v := range best.Value.Parents {
-// 	grid[v.Row][v.Col] = 0
-// }
-// for i, row := range grid {
-// 	for j := range row {
-// 		fmt.Print(grid[i][j])
-// 	}
-// 	fmt.Print("\n")
-// }
+	for _, v := range mods {
+		newRow = current.Value.Row + v.rowMod
+		newCol = current.Value.Col + v.colMod
+		if utils.InBound(newRow, newCol, rows, cols) {
+			res = append(res, Pos{newRow, newCol, v.dir})
+		}
+	}
 
-// cost = make([][]int, len(grid))
-// cols := len(grid[0])
-
-// for i := range cost {
-// 	cost[i] = make([]int, cols)
-// 	for j := range cost[i] {
-// 		cost[i][j] = math.MaxInt32
-// 	}
-// }
+	return res
+}
