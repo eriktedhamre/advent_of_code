4ad42c2e92d882f859ba33b41f3a16524036b394
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2025-10-06 20:38:03 +0200

    day19 part one

--- 2023/day19/main.go
+++ 2023/day19/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
 type axis int
+
 const (
 	x axis = iota
 	m
@@ -18,20 +20,21 @@ const (
 )
 
 type operator int
+
 const (
 	smaller operator = iota
-	greater 
+	greater
 )
 
 type constraint struct {
-	ax axis
-	op operator
-	limit int
-	dest string
+	ax    axis
+	op    operator
+	limit uint64
+	dest  string
 }
 
-type rating struct {
-	xValue, mValue, aValues, sValue int
+type partRating struct {
+	xValue, mValue, aValue, sValue uint64
 }
 
 // If there's cycles again, I'm gonna be so mad :)
@@ -56,27 +59,99 @@ func main() {
 
 func partOne(file *os.File) uint64 {
 	var workflows = make(map[string][]constraint, 0)
+	var parts = make([]partRating, 0)
 	var line string
-	var splits []string
 
 	scanner := bufio.NewScanner(file)
+	// Read Workflows
 	for scanner.Scan() {
 		line = scanner.Text()
-		splits = strings.Split(line, " ")
-		mod, err := strconv.Atoi(splits[1])
-		if err != nil {
-			fmt.Print(err)
-			panic(err)
+		if line == "" {
+			break
+		}
+
+		readWorkflow(line, workflows)
+	}
+
+	//fmt.Println(workflows)
+
+	// If number of iterations for a part is larger than the number of parts abort
+
+	for scanner.Scan() {
+		line = scanner.Text()
+		parts = append(parts, readPart(line))
+	}
+
+	//fmt.Println(parts)
+
+	var sum uint64 = 0
+
+	dest := "in"
+	steps := 0
+	for i := 0; i < len(parts); i++ {
+		dest = "in"
+		steps = 0
+	PART:
+		for {
+			dest = findDest(parts[i], workflows[dest])
+			steps++
+			if dest == "A" {
+				sum += parts[i].aValue + parts[i].xValue + parts[i].mValue + parts[i].sValue
+			}
+			if (dest == "R") || (steps > (len(workflows) + 1)) {
+				break PART
+			}
 		}
 	}
-	return 0
+	return sum
 }
 
-func readWorkflow(input string, workflows map[string][]constraint)  {
+func findDest(part partRating, constraints []constraint) string {
+	dest := ""
+	var limit uint64 = 0
+DONE:
+	for _, c := range constraints {
+		switch c.ax {
+		case x:
+			limit = part.xValue
+		case m:
+			limit = part.mValue
+		case a:
+			limit = part.aValue
+		case s:
+			limit = part.sValue
+		case none:
+			dest = c.dest
+			break DONE
+		}
+		dest = cmprLimit(limit, c.op, c.limit, c)
+		if dest != "" {
+			break DONE
+		}
+	}
+	return dest
+}
+
+func cmprLimit(partValue uint64, op operator, conValue uint64, con constraint) string {
+	dest := ""
+	switch op {
+	case greater:
+		if partValue > conValue {
+			dest = con.dest
+		}
+	case smaller:
+		if partValue < conValue {
+			dest = con.dest
+		}
+	}
+	return dest
+}
+
+func readWorkflow(input string, workflows map[string][]constraint) {
 	keySplits := strings.SplitAfterN(input, "{", 2)
 	key, found := strings.CutSuffix(keySplits[0], "{")
 	if !found {
-		fmt.Errorf("workflow does not begin with XXX{")
+		log.Fatalf("workflow does not begin with XXX{")
 	}
 	noBrackets := strings.TrimSuffix(keySplits[1], "}")
 	workflowSplits := strings.Split(noBrackets, ",")
@@ -106,12 +181,45 @@ func readWorkflow(input string, workflows map[string][]constraint)  {
 			case '>':
 				op = greater
 			}
-			limit, dest, _ := strings.Cut(v[2:], ":")
-			limit, err := strconv.Atoi(limit)
+			limit, dest, found := strings.Cut(v[2:], ":")
+			if !found {
+				log.Fatalf(": not found for constraint")
+				panic(0)
+			}
+			limitValue, err := strconv.ParseUint(limit, 10, 64)
+			if err != nil {
+				log.Fatalf("limitValue int conversion failed")
+				panic(err)
+			}
 
-			mapValues = append(mapValues, constraint{ax: ax, op: op, limit: limit, })
+			mapValues = append(mapValues,
+				constraint{ax: ax, op: op, limit: limitValue, dest: dest})
 		}
-		// if x>2440:R
 	}
 	workflows[key] = mapValues
+}
+
+func readPart(input string) partRating {
+	splits := strings.Split(input, ",")
+	splits[0] = strings.TrimPrefix(splits[0], "{")
+	splits[3] = strings.TrimSuffix(splits[3], "}")
+	part := partRating{}
+	for _, v := range splits {
+		val, err := strconv.ParseUint(v[2:], 10, 64)
+		if err != nil {
+			log.Fatalf("part axis int conversion failed")
+		}
+		switch v[0] {
+		case 'x':
+			part.xValue = val
+		case 'm':
+			part.mValue = val
+		case 'a':
+			part.aValue = val
+		case 's':
+			part.sValue = val
+		}
+	}
+	return part
+
 }
